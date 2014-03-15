f031eb31bb073fde6bec34bcdff29e665e652256
Tsjibbe <tsjibbe.vergeer@gmail.com>
2014-03-15 15:58:57 +0100

    html to string

--- src/HTMLDownloader.java
+++ src/HTMLDownloader.java
@@ -1,4 +1,18 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class HTMLDownloader {
 	
-}
+	StringBuilder contentBuilder = new StringBuilder(); {
+	try {
+	    BufferedReader in = new BufferedReader(new FileReader("http://www.imdb.com/chart/top.html"));
+	    String str;
+	    while ((str = in.readLine()) != null) {
+	        contentBuilder.append(str);
+	    }
+	    in.close();
+	} catch (IOException e) {
+	}
+	String content = contentBuilder.toString();
+}}
